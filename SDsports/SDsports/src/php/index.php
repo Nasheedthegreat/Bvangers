436963eaf3da1ae31e43ba98e634ecaf1f5de352
Nasheedthegreat <144008345+Nasheedthegreat@users.noreply.github.com>
2024-02-08 09:29:06 +0100

    Add files via upload

---
+++ SDsports/SDsports/src/php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> LOGIN </title>
+    <link rel="stylesheet" type="text/css" href="../styles/login.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+<body>
+    <fieldset>
+    <form action="login.php" method="post">
+        <h2>LOGIN</h2><br>
+        <i class="fas fa-sign-in-alt"></i><br>
+        <label>Username</label>
+        <input type="text" name="uname" placeholder="Username"><br>
+        <label>Password</label>
+        <input id="pass" type="password" name="password" placeholder="Password"><br>
+        <button type="submit">Login</button>
+    </form>
+    </fieldset>
+</body>
+</html> 
