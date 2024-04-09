0fd1c14d903ec3b2cac89d2a42a67dff8fec1f87
Nasheedthegreat <z10052004@gmail.com>
2024-04-09 08:51:22 +0200

    login systeem 1
    1

---
+++ SDsports/SDsports/src/php/indextest.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+    <title> SDsports homepage </title>
+    <link rel="stylesheet" href="../styles/admin.css">
+    </head>
+    <body>
+        <!-- menu section start -->
+        <div class="menu">
+            <div class="wrapper">
+           <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="Producten.html">admin manager</a></li>
+            <li><a href="shoppingcart.php">caterogy</a></li>
+            <li><a href="inloggen">Schoenen</a></li>
+            </ul>
+            </div>
+            </div> 
+
+        <!-- menu section end -->
+        <!-- main content section start -->
+        <div class="main-content">
+        <div class="wrapper">
+           Main content goes here
+            </div>
+            </div> 
+        <!-- main content section ends -->
+        <!-- footer section start -->
+        <div class="footer">
+        <div class="wrapper">
+         All rights reserved. 2024. SDsports. Designed by bvengers <a href="#">CSE485</a>
+            </div>
+            </div> 
+        <!-- footer section ends -->
+
+    </body>
+ </html>    
---
+++ SDsports/SDsports/src/php/pdo.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>PHP Data Objects</title>
+</head>
+<body>
+    <h1>PHP Data Objects</h1>
+    <?php
+    // Connect to the database
+    $dsn = 'mysql:host=localhost;dbname=example';
+    $username = 'username'; 
+    $pasword = 'pasword';
+    try {
+        $db = new PDO ($dsn, $username, $pasword);
+        echo '<p>You are connected to the database!</p>';
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        echo "<p>An error occurred while connecting to the database: $error_message</p>";
+    }
+    ?>
+</body>
+</html>
+ 
